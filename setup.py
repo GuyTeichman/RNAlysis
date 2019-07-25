0f18610e7b0ffd91dda38d99e26c3b732731cfab
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2019-07-25 14:59:41 +0300

    updated documentation

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/GuyTeichman/RNAlysis',
-    version='1.0.3',
+    version='1.0.4',
     zip_safe=False,
 )
