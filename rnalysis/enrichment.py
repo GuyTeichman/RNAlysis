f40daa78bb524adf53a7832ade9563207c8724b1
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2019-11-06 21:17:44 +0200

    fixed a bug where an error is raised if enrichment_randomization attributes are 'all'

--- rnalysis/enrichment.py
+++ rnalysis/enrichment.py
@@ -411,7 +411,7 @@ class EnrichmentProcessing:
         big_table, gene_set = self._enrichment_get_reference(biotype=biotype, background_genes=background_genes,
                                                              ref_path=ref_path)
         if attributes == ['all']:
-            attributes = big_table.columns
+            attributes = big_table.columns[:-1]
         fraction = lambda mysrs: (mysrs.shape[0] - mysrs.isna().sum()) / mysrs.shape[0]
         client = Client()
         dview = client[:]
@@ -443,7 +443,6 @@ class EnrichmentProcessing:
 
         return res_df
 
-    # TODO: allow 'all' in attributes to calculate enrichment for all attributes in the table
     def enrich_randomization(self, attributes: list = None, fdr: float = 0.05, reps=10000,
                              biotype: str = 'protein_coding', background_genes: set = None,
                              ref_path: str = 'predefined', save_csv: bool = False, fname=None):
