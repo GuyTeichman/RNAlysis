0a9a2c042d3605435ed5fe7af6ecfacd6acead4d
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-05-21 13:19:39 +0300

    added __all__ and __license__

--- rnalysis/__init__.py
+++ rnalysis/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for sRNA analysis pipeline."""
-import os
 
+__all__ = ['general', 'filtering', 'enrichment']
 __name__ = "rnalysis"
 __author__ = """Guy Teichman"""
-__email__ = 'guyteichman@gmail.com'
-__version__ = '1.3.4'
-__attr_file_key__ = 'attribute_reference_table'
-__biotype_file_key__ = 'biotype_reference_table'
-
+__email__ = "guyteichman@gmail.com"
+__version__ = "1.3.4"
+__license__ = "MIT"
+__attr_file_key__ = "attribute_reference_table"""
+__biotype_file_key__ = "biotype_reference_table"
