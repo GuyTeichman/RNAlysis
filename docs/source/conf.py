92fa22b17f0a439ae9fcd53bd11d62bda818d24c
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-05-21 16:41:29 +0300

    added copybutton functionality to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+
+def setup(app):
+    app.add_js_file('copybutton.js')
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
