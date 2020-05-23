4bfc2c25f2558ef291679a866feb08bc4484fec1
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-05-23 14:01:49 +0300

    updated relative path for sphinx autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('rnalysis'))
+#sys.path.insert(0, os.path.abspath('rnalysis'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
