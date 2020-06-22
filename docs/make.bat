091a0378a89f524b73243063a0c6cb2e4a820e1a
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-06-22 19:16:00 +0300

    updated sourcedir

--- docs/make.bat
+++ docs/make.bat
@@ -7,7 +7,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=python -msphinx
 )
-set SOURCEDIR=.
+set SOURCEDIR=source
 set BUILDDIR=_build
 set SPHINXPROJ=rnalysis
 
