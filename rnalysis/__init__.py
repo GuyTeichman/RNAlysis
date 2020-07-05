e79982247daf4384190a119a56961c5aa369dd33
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-07-05 17:36:55 +0300

    warnings are now raised in a simple format (warning message only)

--- rnalysis/__init__.py
+++ rnalysis/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for sRNA analysis pipeline."""
+import warnings
 
 __all__ = ['general', 'filtering', 'enrichment', '__attr_file_key__', '__biotype_file_key__']
 __name__ = "rnalysis"
@@ -10,3 +11,11 @@ __version__ = "1.3.4"
 __license__ = "MIT"
 __attr_file_key__ = "attribute_reference_table"""
 __biotype_file_key__ = "biotype_reference_table"
+
+
+def _simple_warning_format(msg, *args, **kwargs):
+    # ignore everything except the warning message
+    return f"Warning: {msg}\n"
+
+
+warnings.formatwarning = _simple_warning_format
