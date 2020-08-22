cdee7a0eeb85b22a3f20ac023551f631a5685709
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-08-22 16:57:20 +0300

    modified test setup

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,9 @@
+import os
+
+try:
+    os.chdir('tests/')
+except FileNotFoundError:
+    pass
+
 __attr_ref__ = 'test_files/attr_ref_table_for_tests.csv'
 __biotype_ref__ = 'test_files/biotype_ref_table_for_tests.csv'
