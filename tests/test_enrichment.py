33089a3f23ec80cbb59f2c982a3c466ecf62bed5
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-08-25 17:50:49 +0300

    implemented tests for RankedSet.__init__(); updated test for venn_diagram

--- tests/test_enrichment.py
+++ tests/test_enrichment.py
@@ -569,11 +569,20 @@ def test_allm_pvals():
 
 
 def test_rankedset_from_ndarray():
-    assert False
+    gene_list = ['gene1', 'gene2', 'gene3', 'gene13']
+    arr = np.array(gene_list)
+    r = RankedSet(arr)
+    for i, j in zip(gene_list, r.ranked_genes):
+        assert i == j
 
 
 def test_rankedset_init_invalid_type():
-    assert False
+    with pytest.raises(TypeError):
+        _ = RankedSet({1, 2, 3})
+    with pytest.raises(TypeError):
+        _ = RankedSet({1: 2, 3: 4})
+    with pytest.raises(TypeError):
+        _ = RankedSet(5)
 
 
 def test_go_enrichment_fetch_annotations(monkeypatch):
@@ -589,6 +598,10 @@ def test_go_enrichment_output():
 
 
 def test_go_enrichment_api():
+    assert False
+
+
+def test_enrichment_output():
     assert False
 
 
@@ -644,9 +657,13 @@ def test_upset_plot_api():
 
 def test_venn_diagram_api():
     venn_diagram({'obj': {'0', '1', '2'}, 'obj2': {'1', '3'}, 'obj3': {'5', '6', '7', '0'}})
-    venn_diagram({'obj': {'0', '1', '2'}, 'obj2': FeatureSet({'1', '2', '3'}), 'obj3': RankedSet(['5', '3', '0'])})
+    venn_diagram({'obj': {'0', '1', '2'}, 'obj2': FeatureSet({'1', '2', '3'}), 'obj3': RankedSet(['5', '3', '0'])},
+                 alpha=0.2, set_colors=('black', 'purple', 'yellow'), linestyle='dashed', linewidth=1, linecolor='grey',
+                 title='title')
     venn_diagram({'obj': 'attribute1', 'obj2': 'attribute2', 'obj3': {'WBGene00000001', 'WBGene00001234'}},
-                 ref=__attr_ref__)
+                 ref=__attr_ref__, lines=False)
+    venn_diagram({'obj': {'0', '1', '2'}, 'otherobj': {'2', '3', '5'}})
+    venn_diagram({'obj': {'0', '1', '2'}, 'otherobj': {'2', '3', '5'}}, weighted=False)
     plt.close('all')
 
 
@@ -676,7 +693,3 @@ def test_generate_upset_srs():
         {'a': {'1', '2', '3', '6'}, 'b': {'2', '3', '4', '5', '6'}, 'c': {'1', '5', '6'}}).sort_index()
     assert srs.index.sort_values().equals(multi_index_truth)
     assert srs.sort_index().equals(srs_truth.sort_index())
-
-
-def test_enrichment_output():
-    assert False
