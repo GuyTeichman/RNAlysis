e899ed4594b1c072a9c265b70d6c247811b78070
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-04-05 15:09:54 +0300

    sparse_dict_to_bool_df() no longer indexes DataFrames with sets

--- rnalysis/utils/parsing.py
+++ rnalysis/utils/parsing.py
@@ -151,7 +151,7 @@ def sparse_dict_to_bool_df(sparse_dict: Dict[str, set], progress_bar_desc: str =
         columns.update(val)
     df = pd.DataFrame(np.zeros((len(rows), len(columns)), dtype=bool), columns=columns, index=rows)
     for row, col in zip(tqdm(sparse_dict.keys(), desc=progress_bar_desc, bar_format=fmt), sparse_dict.values()):
-        df.loc[row, col] = True
+        df.loc[row, list(col)] = True
     return df
 
 
