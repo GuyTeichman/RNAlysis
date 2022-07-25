2415c0be1de14a3ae49bd8ba10abc6fc8d65e76a
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-07-25 17:07:05 +0300

    added tests for get_icon()

--- tests/test_gui_graphics.py
+++ tests/test_gui_graphics.py
@@ -1,3 +1,29 @@
 import pytest
 from pytestqt import qtbot
 from rnalysis.gui.gui_graphics import *
+
+
+def test_get_icon_invalid(qtbot):
+    icon = get_icon('something invalid')
+    assert icon is None
+
+
+def test_get_icon_blank(qtbot):
+    pixmap = QtGui.QPixmap(32, 32)
+    pixmap.fill(QtCore.Qt.transparent)
+    truth = pixmap.toImage()
+
+    icon = get_icon('blank')
+
+    assert icon.pixmap(32, 32).toImage() == truth
+
+
+@pytest.mark.parametrize("name,path", [
+    ('yellow', 'yellow_icon.png'),
+    ('Filter', 'filter_icon.png')])
+def test_get_icon(qtbot, name, path):
+    full_path = 'gui/icons/' + path
+    icon = get_icon(name)
+    assert isinstance(icon, QtGui.QIcon)
+    assert not icon.isNull()
+    assert icon.name() == QtGui.QIcon(full_path).name()
