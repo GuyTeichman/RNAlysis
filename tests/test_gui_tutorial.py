411725810d99ba590f065da1b3514086429b8ee6
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-08-16 16:57:52 +0300

    implemented test for WelcomeWizard

--- tests/test_gui_tutorial.py
+++ tests/test_gui_tutorial.py
@@ -4,6 +4,13 @@ LEFT_CLICK = QtCore.Qt.LeftButton
 RIGHT_CLICK = QtCore.Qt.RightButton
 
 
+def widget_setup(qtbot, widget_class, *args, **kwargs):
+    widget = widget_class(*args, **kwargs)
+    widget.show()
+    qtbot.add_widget(widget)
+    return qtbot, widget
+
+
 def test_TutorialMovie_init(qtbot):
     assert False
 
@@ -13,8 +20,11 @@ def test_TutorialMovie_buttons(qtbot):
 
 
 def test_WelcomeWizard_init(qtbot):
-    assert False
+    qtbot, window = widget_setup(qtbot, WelcomeWizard)
+    for i in range(len(window.TITLES) + 1):
+        qtbot.mouseClick(window.button(QtWidgets.QWizard.NextButton), LEFT_CLICK)
+    qtbot.mouseClick(window.button(QtWidgets.QWizard.FinishButton), LEFT_CLICK)
 
 
-def test_WelcomeWizard_do_not_show_again(qtbot):
+def test_WelcomeWizard_do_not_show_again_saved(qtbot):
     assert False
