5f0106941a058af4ed841c95d9785cecded1130b
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-08-19 12:03:14 +0300

    implemented tests and test placeholders for SetTabPage and FilterTabPage

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,4 +1,6 @@
 import pytest
+
+import filtering
 from rnalysis.gui.gui import *
 
 LEFT_CLICK = QtCore.Qt.LeftButton
@@ -81,10 +83,125 @@ def test_SetVisualizationWindowinit(qtbot):
 
 
 def test_FilterTabPage_init(qtbot):
+    qtbot, window = widget_setup(qtbot, FilterTabPage)
+
+
+def test_FilterTabPage_load_file(qtbot):
+    obj_truth = filtering.CountFilter('tests/test_files/counted.csv')
+    qtbot, window = widget_setup(qtbot, FilterTabPage)
+    assert window.is_empty()
+    assert not window.basic_widgets['start_button'].isEnabled()
+
+    window.basic_widgets['file_path'].clear()
+    qtbot.keyClicks(window.basic_widgets['file_path'].file_path, str(Path('tests/test_files/counted.csv').absolute()))
+    qtbot.keyClicks(window.basic_widgets['table_type_combo'], 'Count matrix')
+    qtbot.mouseClick(window.basic_widgets['start_button'], LEFT_CLICK)
+
+    assert not window.is_empty()
+    assert window.obj() == obj_truth
+    assert window.obj_type() == filtering.CountFilter
+    assert window.name == 'counted'
+    assert window.get_table_type() == 'Count matrix'
+
+
+def test_FilterTabPage_from_obj(qtbot):
+    table_name = 'table name'
+    qtbot, window = widget_setup(qtbot, FilterTabPage)
+    obj = filtering.DESeqFilter('tests/test_files/test_deseq.csv')
+    assert window.is_empty()
+    window.start_from_filter_obj(obj, table_name)
+
+    assert not window.is_empty()
+    assert window.obj() == obj
+    assert window.obj_type() == filtering.DESeqFilter
+    assert window.name == table_name
+    assert window.get_table_type() == 'Differential expression'
+
+
+def test_FilterTabPage_cache(qtbot):
+    assert False
+
+
+def test_FilterTabPage_obj_properties(qtbot):
+    assert False
+
+
+def test_FilterTabPage_rename(qtbot):
+    assert False
+
+
+def test_FilterTabPage_undo_rename(qtbot):
+    assert False
+
+
+def test_FilterTabPage_save_table(qtbot):
+    assert False
+
+
+def test_FilterTabPage_view_full_table(qtbot):
+    assert False
+
+
+def test_FilterTabPage_apply_function(qtbot):
+    assert False
+
+
+def test_FilterTabPage_undo_function(qtbot):
+    assert False
+
+
+def test_FilterTabPage_apply_pipeline(qtbot):
+    assert False
+
+
+def test_FilterTabPage_undo_pipeline(qtbot):
+    assert False
+
+
+def test_FilterTabPage_get_all_actions(qtbot):
     assert False
 
 
 def test_SetTabPage_init(qtbot):
+    qtbot, window = widget_setup(qtbot, SetTabPage, 'set name')
+    qtbot, window = widget_setup(qtbot, SetTabPage, 'set name', {'aa', 'bb', 'cc', 'dd'})
+
+
+def test_SetTabPage_from_set(qtbot):
+    set_name = 'table name'
+    qtbot, window = widget_setup(qtbot, SetTabPage, set_name)
+    assert window.is_empty()
+
+    obj = {'abc', 'def', 'ghi', 'jkl'}
+    window.update_gene_set(obj)
+
+    assert not window.is_empty()
+    assert window.obj() == obj
+    assert window.obj_type() == set
+    assert window.name == set_name
+
+
+def test_SetTabPage_cache(qtbot):
+    assert False
+
+
+def test_SetTabPage_obj_properties(qtbot):
+    assert False
+
+
+def test_SetTabPage_rename(qtbot):
+    assert False
+
+
+def test_SetTabPage_undo_rename(qtbot):
+    assert False
+
+
+def test_SetTabPage_save_table(qtbot):
+    assert False
+
+
+def test_SetTabPage_view_full_table(qtbot):
     assert False
 
 
