6ac79c897750e81a333236d02b68e2455b348c23
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-10-03 17:41:52 +0300

    added GUI tutorial to user guide

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+sys.path.insert(0, os.path.abspath('../../rnalysis/gui/videos'))
+
 
 # sys.path.insert(0, os.path.abspath('rnalysis'))
 sys.path.insert(0, os.path.abspath('../..'))
