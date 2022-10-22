0b483ca91d8d8d90691058a4c33cf8dff2e6a2af
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-10-22 23:27:18 +0300

    fixup! fixup! added more functions to the "general" category

--- tests/test_filtering.py
+++ tests/test_filtering.py
@@ -122,7 +122,16 @@ def test_from_string(monkeypatch):
     ('UniProtKB AC/ID', 'WormBase', False, 'tests/test_files/counted_translated_with_unmapped.csv'),
     ('UniProtKB', 'auto', True, 'tests/test_files/counted_translated_remove_unmapped.csv'),
 ])
-def test_filter_translate_gene_ids(map_to, map_from, remove_unmapped_genes, expected):
+def test_filter_translate_gene_ids(map_to, map_from, remove_unmapped_genes, expected, monkeypatch):
+    def mock_map_gene_ids(ids, trans_from, trans_to='UniProtKB AC', verbose=True):
+        if trans_from == 'WormBase':
+            return {'WBGene00007063': 'A0A0K3AWR5', 'WBGene00007064': 'A0A2X0T1Z3', 'WBGene00007067': 'D3NQA2',
+                    'WBGene00077503': 'H2L2B5', 'WBGene00007071': 'Q17405', 'WBGene00014997': 'Q7JNR0',
+                    'WBGene00043988': 'A4F2Z7', 'WBGene00043989': 'G5EFZ2', 'WBGene00007075': 'G5EDW3',
+                    'WBGene00007076': 'G5EFZ2'}
+        return {}
+
+    monkeypatch.setattr(io, 'map_gene_ids',mock_map_gene_ids)
     truth = io.load_csv(expected, index_col=0)
     f = Filter('tests/test_files/counted.csv')
 
