8804f7f28d27b138ba3508f85f3c02396ef5e6ba
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-10-29 13:45:49 +0300

    added docstring to cutadapt functions

--- rnalysis/fastq.py
+++ rnalysis/fastq.py
@@ -25,6 +25,46 @@ def trim_adapters_single_end(fastq_folder: Union[str, Path], output_folder: Unio
                              minimum_read_length: Union[int, None] = 10, maximum_read_length: Union[int, None] = None,
                              discard_untrimmed_reads: bool = True, error_tolerance: float = 0.1,
                              minimum_overlap: int = 3, allow_indels: bool = True, parallel: bool = True):
+    """
+    Trim adapters from single-end reads using `CutAdapt <https://cutadapt.readthedocs.io/>`_.
+
+    :param fastq_folder: Path to the folder containing your untrimmed FASTQ files
+    :type fastq_folder: str/Path to an existing folder
+    :param output_folder: Path to a folder in which the trimmed FASTQ files, as well as the log files, will be saved.
+    :type output_folder: str/Path to an existing folder
+    :param three_prime_adapters: the sequence of the adapter/adapters to trim from the 3' end of the reads.
+    :type three_prime_adapters: str, list of str, or None
+    :param five_prime_adapters: the sequence of the adapter/adapters to trim from the 5' end of the reads.
+    :type five_prime_adapters: str, list of str, or None (default=None)
+    :param any_position_adapters: the sequence of the adapter/adapters to trim from either end \
+    (or from the middle) of the reads.
+    :type any_position_adapters: str, list of str, or None (default=None)
+    :param quality_trimming: if specified, trim low-quality 3' end from the reads. Any bases with quality score below \
+    the specified value will be trimmed from the 3' end of the read.
+    :type quality_trimming: int or None (default=20)
+    :param trim_n: if True, removem flanking N bases from each read. For example, a read with the sequence \
+    'NNACGTACGTNNNN' will be trimmed down to 'ACGTACGT'. This occurs after adapter trimming.
+    :type trim_n: bool (default=True)
+    :param minimum_read_length: if specified (default), \
+    discard processed reads that are shorter than minimum_read_length.
+    :type minimum_read_length: int or None (default=10)
+    :param maximum_read_length: if specified, discard processed reads that are shorter than minimum_read_length.
+    :type maximum_read_length: int or None (default=None)
+    :param discard_untrimmed_reads: if True, discards reads in which no adapter was found.
+    :type discard_untrimmed_reads: bool (default=True)
+    :param error_tolerance: The level of error tolerance permitted when searching for adapters, with the lowest \
+    value being 0 (no error tolerance) and the maximum being 1 (100% error tolerance). \
+    Allowed errors are mismatches, insertions and deletions.
+    :type error_tolerance: float between 0 and 1 (default=0.1)
+    :param minimum_overlap: the minimum number of nucleotides that must match exactly to the adapter sequence \
+    in order to trim it.
+    :type minimum_overlap: int >= 0 (default=3)
+    :param allow_indels: if False, insertions and deletions in the adapter sequence are not allowed - only mismatches.
+    :type allow_indels: bool (default=True)
+    :param parallel: if True, runs CutAdapt on all available cores in parallel. \
+    Otherwise, run CutAdapt on a single processor only.
+    :type parallel: bool (default=True)
+    """
     if not HAS_CUTADAPT:
         warnings.warn(f"Python package 'cutadapt' is not installed. \n"
                       f"If you want to use the adapter trimming feature, "
@@ -78,6 +118,68 @@ def trim_adapters_paired_end(r1_files: List[Union[str, Path]], r2_files: List[Un
                              pair_filter_if: Literal['both', 'any', 'first'] = 'both',
                              error_tolerance: float = 0.1, minimum_overlap: int = 3, allow_indels: bool = True,
                              parallel: bool = True):
+    """
+    Trim adapters from paired-end reads using `CutAdapt <https://cutadapt.readthedocs.io/>`_.
+
+    :param r1_files: a list of paths to your Read#1 files. The files should be sorted in tandem with r2_files, \
+    so that they line up to form pairs of R1 and R2 files.
+    :type r1_files: list of str/Path to existing FASTQ files
+    :param r2_files: a list of paths to your Read#2 files. The files should be sorted in tandem with r1_files, \
+    so that they line up to form pairs of R1 and R2 files.
+    :type r2_files: list of str/Path to existing FASTQ files
+    :param output_folder: Path to a folder in which the trimmed FASTQ files, as well as the log files, will be saved.
+    :type output_folder: str/Path to an existing folder
+    :param three_prime_adapters_r1: the sequence of the adapter/adapters \
+    to trim from the 3' end of the reads in Read#1 files.
+    :type three_prime_adapters_r1: str, list of str, or None
+    :param three_prime_adapters_r2: the sequence of the adapter/adapters \
+    to trim from the 3' end of the reads in Read#2 files.
+    :type three_prime_adapters_r2: str, list of str, or None
+    :param five_prime_adapters_r1: the sequence of the adapter/adapters \
+    to trim from the 5' end of the reads in Read#1 files.
+    :type five_prime_adapters_r1: str, list of str, or None (default=None)
+    :param five_prime_adapters_r2: the sequence of the adapter/adapters \
+    to trim from the 5' end of the reads in Read#2 files.
+    :type five_prime_adapters_r2: str, list of str, or None (default=None)
+    :param any_position_adapters_r1: the sequence of the adapter/adapters \
+    to trim from either end (or the middle) of the reads in Read#1 files.
+    :type any_position_adapters_r1: str, list of str, or None (default=None)
+    :param any_position_adapters_r2: the sequence of the adapter/adapters \
+    to trim from either end (or the middle) of the reads in Read#2 files.
+    :type any_position_adapters_r2: str, list of str, or None (default=None)
+    :param quality_trimming: if specified, trim low-quality 3' end from the reads. Any bases with quality score below \
+    the specified value will be trimmed from the 3' end of the read.
+    :type quality_trimming: int or None (default=20)
+    :param trim_n: if True, removem flanking N bases from each read. For example, a read with the sequence \
+    'NNACGTACGTNNNN' will be trimmed down to 'ACGTACGT'. This occurs after adapter trimming.
+    :type trim_n: bool (default=True)
+    :param minimum_read_length: if specified (default), \
+    discard processed reads that are shorter than minimum_read_length.
+    :type minimum_read_length: int or None (default=10)
+    :param maximum_read_length: if specified, discard processed reads that are shorter than minimum_read_length.
+    :type maximum_read_length: int or None (default=None)
+    :param discard_untrimmed_reads: if True, discards reads in which no adapter was found.
+    :type discard_untrimmed_reads: bool (default=True)
+    :param pair_filter_if: Cutadapt always discards both reads of a pair if it determines that the pair \
+    should be discarded. This parameter determines how to combine the filters for Read#1 and Read#2 into a \
+    single decision about the read pair. When the value is 'both', you require that filtering criteria must apply \
+    to both reads in order for a read pair to be discarded. When the value is 'any', you require that \
+    at least one of the reads (R1 or R2) fulfills the filtering criterion in order to discard them. \
+    When the value is 'first', only the first read in each pair determines whether to discard the pair or not.
+    :type pair_filter_if: 'both', 'any', or 'first' (default='both')
+    :param error_tolerance: The level of error tolerance permitted when searching for adapters, with the lowest \
+    value being 0 (no error tolerance) and the maximum being 1 (100% error tolerance). \
+    Allowed errors are mismatches, insertions and deletions.
+    :type error_tolerance: float between 0 and 1 (default=0.1)
+    :param minimum_overlap: the minimum number of nucleotides that must match exactly to the adapter sequence \
+    in order to trim it.
+    :type minimum_overlap: int >= 0 (default=3)
+    :param allow_indels: if False, insertions and deletions in the adapter sequence are not allowed - only mismatches.
+    :type allow_indels: bool (default=True)
+    :param parallel: if True, runs CutAdapt on all available cores in parallel. \
+    Otherwise, run CutAdapt on a single processor only.
+    :type parallel: bool (default=True)
+    """
     if not HAS_CUTADAPT:
         warnings.warn(f"Python package 'cutadapt' is not installed. \n"
                       f"If you want to use the adapter trimming feature, "
