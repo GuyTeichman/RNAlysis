f5ef4f08be911eabc53042af8ca110aef8e2017d
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-11-01 00:03:50 +0200

    DESeq2 install now uses parameters update=TRUE, ask=FALSE

--- rnalysis/utils/r_templates/deseq2_install.R
+++ rnalysis/utils/r_templates/deseq2_install.R
@@ -1,4 +1,4 @@
 if (!require("DESeq2", quietly = TRUE))
     if (!require("BiocManager", quietly = TRUE))
         install.packages("BiocManager")
-    BiocManager::install("DESeq2",force = TRUE)
+    BiocManager::install("DESeq2",update = TRUE, ask=FALSE)
