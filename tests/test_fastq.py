de1e8bff29d49140db20b6a66ed46b5c889d5179
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-11-04 13:54:16 +0200

    fixup! fixup! unit test fixups

--- tests/test_fastq.py
+++ tests/test_fastq.py
@@ -88,7 +88,9 @@ def test_kallisto_quantify_single_end():
         assert are_dir_trees_equal(out_dir, truth_dir)
     finally:
         for item in Path(out_dir).iterdir():
-            if item.is_file() and 'gitignore' not in item.name:
+            if 'gitignore' in item.name:
+                continue
+            if item.is_file():
                 item.unlink()
             else:
                 shutil.rmtree(item)
@@ -109,7 +111,9 @@ def test_kallisto_quantify_paired_end():
 
     finally:
         for item in Path(out_dir).iterdir():
-            if item.is_file() and 'gitignore' not in item.name:
+            if 'gitignore' in item.name:
+                continue
+            if item.is_file():
                 item.unlink()
             else:
                 shutil.rmtree(item)
