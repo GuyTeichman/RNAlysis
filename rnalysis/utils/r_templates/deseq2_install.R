dcf8de633b8e795b4f9c7d1e29f894235ccf879e
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-11-20 17:11:48 +0200

    updated DESeq2 install script conditionals

--- rnalysis/utils/r_templates/deseq2_install.R
+++ rnalysis/utils/r_templates/deseq2_install.R
@@ -1,4 +1,6 @@
-if (!require("DESeq2", quietly = TRUE))
-    if (!require("BiocManager", quietly = TRUE))
+if (("xtable" %in% rownames(installed.packages()) == FALSE) || (!require("DESeq2", quietly = TRUE))) {
+    if (!require("BiocManager", quietly = TRUE)) {
         install.packages("BiocManager")
+        }
     BiocManager::install("DESeq2",update=TRUE, ask=FALSE, force=TRUE)
+}
