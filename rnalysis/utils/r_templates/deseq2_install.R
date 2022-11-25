d31d05f1570c0601a37bfbef8178fe83013103ea
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-11-25 20:42:49 +0200

    added RSQLite to DESeq2 intallation script

--- rnalysis/utils/r_templates/deseq2_install.R
+++ rnalysis/utils/r_templates/deseq2_install.R
@@ -3,5 +3,6 @@ if (("DESeq2" %in% rownames(installed.packages()) == FALSE) || (!require("DESeq2
         install.packages("BiocManager")
         }
     BiocManager::install("DelayedArray",update=TRUE, ask=FALSE, force=TRUE)
+    BiocManager::install("RSQLite",update=TRUE, ask=FALSE, force=TRUE)
     BiocManager::install("DESeq2",update=TRUE, ask=FALSE, force=TRUE)
 }
