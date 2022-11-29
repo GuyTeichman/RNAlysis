958c86faadb0fb7c6f49cfc52a1d09d94a81dfc6
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-11-29 14:47:44 +0200

    added png to DESeq2 installation script

--- rnalysis/utils/r_templates/deseq2_install.R
+++ rnalysis/utils/r_templates/deseq2_install.R
@@ -1,6 +1,7 @@
 if (("DESeq2" %in% rownames(installed.packages()) == FALSE) || (!require("DESeq2", quietly = TRUE))) {
     if (!require("BiocManager", quietly = TRUE)) {
         install.packages("BiocManager")
+        install.packages("png")
         }
     BiocManager::install("DelayedArray",update=TRUE, ask=FALSE, force=TRUE)
     BiocManager::install("RSQLite",update=TRUE, ask=FALSE, force=TRUE)
