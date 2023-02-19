f6d568f3fab1f3046d5040a88970af41eefae020
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2023-02-19 16:08:21 +0200

    added cutadapt call to rnalysis_app.py

--- rnalysis_app.py
+++ rnalysis_app.py
@@ -2,7 +2,9 @@ import asyncio
 from multiprocessing import freeze_support
 
 from rnalysis.gui import gui
+from rnalysis.utils import io
 
 if __name__ == '__main__':
     freeze_support()
+    io.run_subprocess(['cutadapt', '--help'])
     asyncio.run(gui.run())
