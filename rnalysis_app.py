98eb2bb06af9a3225e12d4084a92f0e69675c454
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2023-02-21 11:48:06 +0200

    Revert "added cutadapt call to rnalysis_app.py"
    This reverts commit f6d568f3fab1f3046d5040a88970af41eefae020.

--- rnalysis_app.py
+++ rnalysis_app.py
@@ -2,9 +2,7 @@ import asyncio
 from multiprocessing import freeze_support
 
 from rnalysis.gui import gui
-from rnalysis.utils import io
 
 if __name__ == '__main__':
     freeze_support()
-    io.run_subprocess(['cutadapt', '--help'])
     asyncio.run(gui.run())
