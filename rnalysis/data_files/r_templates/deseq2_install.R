823ff0180ce31b3c7e80a8110304e25916d13f4c
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2023-03-20 13:06:59 +0200

    added XML to DESeq2 install script

--- rnalysis/data_files/r_templates/deseq2_install.R
+++ rnalysis/data_files/r_templates/deseq2_install.R
@@ -2,6 +2,7 @@ if (("DESeq2" %in% rownames(installed.packages()) == FALSE) || (!require("DESeq2
     if (!require("BiocManager", quietly = TRUE)) {
         install.packages("BiocManager")
         install.packages("png")
+        install.packages("XML")
         }
     BiocManager::install("DelayedArray",update=TRUE, ask=FALSE, force=TRUE)
     BiocManager::install("RSQLite",update=TRUE, ask=FALSE, force=TRUE)
