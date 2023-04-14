3d4117ec86c02223f4fa762af2d797a807e1cd6f
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2023-04-14 12:47:40 +0300

    fixup! process_outputs() no longer opens MultiKeepWindow when receiving an empty list

--- rnalysis/gui/gui.py
+++ rnalysis/gui/gui.py
@@ -1375,7 +1375,7 @@ class TabPage(QtWidgets.QWidget):
         self.process_outputs(result, func_name)
 
     def process_outputs(self, outputs, source_name: str = ''):
-        if len(outputs) == 0:
+        if validation.isiterable(outputs) and len(outputs) == 0:
             return
         elif validation.isinstanceinh(outputs, (filtering.Filter, fastq.filtering.Filter)):
             self.filterObjectCreated.emit(outputs)
