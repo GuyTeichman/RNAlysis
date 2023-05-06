8a216ba700d272752399df58997c249f1d35d82e
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2023-05-06 21:03:53 +0300

    test_load_cached_gui_file now removes \r from bytes output

--- tests/test_io.py
+++ tests/test_io.py
@@ -883,9 +883,9 @@ def test_cache_gui_file(item, filename):
     ({"apple", "banana", "cherry"}, "test.txt", True, {"apple", "banana", "cherry"}),
     ("test", "test.txt", True, {"test"}),
     ("test123", "test.txt", False, b"test123"),
-    (pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}), "test.csv", False, b",a,b\r\n0,1,4\r\n1,2,5\r\n2,3,6\r\n"),
+    (pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}), "test.csv", False, b",a,b\n0,1,4\n1,2,5\n2,3,6\n"),
     ({"apple", "banana", "cherry"}, "test.txt", False,
-     bytes("\r\n".join({"apple", "banana", "cherry"}), encoding='utf-8')),
+     bytes("\n".join({"apple", "banana", "cherry"}), encoding='utf-8')),
     ("test", "test.txt", False, b"test")
 ])
 def test_load_cached_gui_file(item, filename, load_as_obj, expected_output):
@@ -899,6 +899,8 @@ def test_load_cached_gui_file(item, filename, load_as_obj, expected_output):
         res = load_cached_gui_file(filename, load_as_obj)
         if isinstance(res, pd.DataFrame):
             assert res.equals(item)
+        elif isinstance(res, bytes):
+            assert res.replace(b'\r', b'') == expected_output
         else:
             assert res == expected_output
     finally:
