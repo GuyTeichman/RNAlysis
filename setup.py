7625ef06eb38a07cbadc484bdcd66cf1ffcc18d3
guyteichman <guyteichman@gmail.com>
2025-01-11 15:12:34 +0200

    Bump version: 4.1.0 → 4.1.1

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     tests_require=test_requirements,
     extras_require=extras_require,
     url='https://github.com/GuyTeichman/RNAlysis',
-    version='4.1.0',
+    version='4.1.1',
     zip_safe=False,
     entry_points={
         'console_scripts': [
