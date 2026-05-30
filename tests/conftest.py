002165cad2fce324286ad8fec99aadef54d4a0c7
guyteichman <guyteichman@gmail.com>
2026-05-30 16:10:46 +0300

    force macos to render tests off-screen

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+# Force headless offscreen rendering for Qt on macOS CI environments
+if sys.platform == 'darwin':
+    os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
 
 def pytest_configure(config):
