c16b014e1252c6269dca70b4395ad27d9e5aa129
guyteichman <guyteichman@gmail.com>
2026-05-30 23:55:59 +0300

    Bump version: 4.1.2 → 4.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = '2024, Guy Teichman'
 author = 'Guy Teichman'
 
 # The full version, including alpha/beta/rc tags
-release = '4.1.2'
+release = '4.2.0'
 
 # -- General configuration ---------------------------------------------------
 
